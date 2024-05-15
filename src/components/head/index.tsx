26e225a76ee65c3861742e75d5c74c5fd80f0a34
Radium <2043869032@qq.com>
2024-05-15 23:47:18 +0800

    chore: op code

--- src/components/head/index.tsx
+++ src/components/head/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import Styles from './index.module.scss';
 import clasnames from 'classnames';
+const backgroundImg = process.env.PUBLIC_URL + './header-background.png';
 
 const Head: React.FC = () => {
   return (
     <div className={clasnames(Styles.header)}>
       <div className={Styles.headerIcon}>
-        <img src="" className={clasnames('bg-white', '')} alt="" />
+        <img className={clasnames('bg-white', '')} alt="" />
+
+        <span>今日月阅读时长</span>
       </div>
+      <img src={backgroundImg} className={Styles.background} alt="" />
     </div>
   );
 };
