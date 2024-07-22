f9791bf6ee62e01e40c4872b448e8f09a6b4df2e
zly01858966 <zly01858966@lazada.com>
2024-07-22 17:26:14 +0800

    chore: op code

--- src/pages/pdf/index.tsx
+++ src/pages/pdf/index.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 
 const Pdf: React.FC = () => {
-  const  [state,setState] = React.useState(1)
-  return <div onClick={()=>{setState(state+1)}}>{state}</div>;
+  const [state, setState] = React.useState(1);
+  return (
+    <div
+      onClick={() => {
+        setState(state + 1);
+      }}
+    >
+      {state}
+    </div>
+  );
 };
 
 export default Pdf;
